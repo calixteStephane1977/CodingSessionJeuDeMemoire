5eb59585049d0d5c6a683a73b189e9f9be9db5d8
calixteStephane1977 <126086360+calixteStephane1977@users.noreply.github.com>
2023-03-30 16:44:12 +0200

    commit

--- index.js
+++ index.js
@@ -1,94 +1,266 @@
-const body =document.querySelector('body');
-console.log(body);
-
-// const array_pics={
-//         goku:" ./img/goku.png ",
-//         gon:" ./img/gon.png ",
-//         happy:" ./imghappy.png ",
-//         luffy:" ./img/luffy.png ",
-//         naruto:" ./img/naruto.png ",
-//         shinichi:" ./img/shinichi.png "
-
-// };
-const array_pics=[
-
-    " ./img/goku.png ",
-    " ./img/gon.png ",
-    " ./imghappy.png ",
-    " ./img/luffy.png ",
-    " ./img/naruto.png ",
-    " ./img/shinichi.png "
-]
-
-;
-console.log(array_pics);
-
-
-const container ={
-    container:document.createElement("div")
-}
-
-const newDiv = {
-    card_1:document.createElement('div'),
-    card_2:document.createElement('div'),
-    card_3:document.createElement('div'),
-    card_4:document.createElement('div'),
-    card_5:document.createElement('div'),
-    card_6:document.createElement('div'),
-    card_7:document.createElement('div'),
-    card_8:document.createElement('div'),
-    card_9:document.createElement('div'),
-    card_10:document.createElement('div'),
-    card_11:document.createElement('div'),
-    card_12:document.createElement('div'),
-}
-for (const [key,value] of Object.entries(newDiv)) {
-    console.log(value);
-    value.setAttribute("id",key)
-    value.setAttribute("class","card")
-
-    // value.innerHTML=`<img src=${array_pics.goku} alt="">`
-    // value.innerHTML=`<img src=${array_pics.gon} alt="">`
-    // value.innerHTML=`<img src=${array_pics.luffy} alt="">`
-    // value.innerHTML=`<img src=${array_pics.happy} alt="">`
-    // value.innerHTML=`<img src=${array_pics.naruto} alt="">`
-    // value.innerHTML=`<img src=${array_pics.shinichi} alt="">`
-    container.container.appendChild(value)
-}   
-// =============================container
-container.container.setAttribute("class",'main_container');
-body.appendChild(container.container);
 const cardImgs = document.querySelectorAll('.card');
-console.log(cardImgs)
-// =======================================pics
-     for (let i = 0; i < array_pics.length; i++) {
-        const element = array_pics[i];
-        console.log(element);
-     }
+const body =document.querySelector('body');
+const con = document.getElementsByTagName('article');
+console.log(con);
+// console.log(body);
+// ====================================
+    
+// const array_pics = [
+//         " ./img/goku.png ",   
+//         " ./img/gon.png " ,
+//          " ./img/happy.png " ,
+//         " ./img/luffy.png " ,
+//          " ./img/naruto.png " ,
+//          " ./img/shinichi.png " 
+//         ];
+let array_pics = [
+        " ./img/goku.png ",   
+        " ./img/gon.png " ,
+         " ./img/happy.png " ,
+        " ./img/luffy.png " ,
+         " ./img/naruto.png " ,
+         " ./img/shinichi.png ", 
+        " ./img/goku.png ",   
+        " ./img/gon.png " ,
+         " ./img/happy.png " ,
+        " ./img/luffy.png " ,
+         " ./img/naruto.png " ,
+         " ./img/shinichi.png " 
+        ];
+// let array_pics = [
+//         { pic:"goku"},   
+//         { pic:"gon"} ,
+//         { pic:"happy"} ,
+//         { pic:"luffy"} ,
+//         { pic:"naruto"} ,
+//         { pic:"shinichi"}, 
+//         // { pic:goku.png},   
+//         // { pic:gon} ,
+//         // { pic:happy} ,
+//         // { pic:luffy} ,
+//         // { pic:naruto} ,
+//         // { pic:shinichi} 
+//         ];
+        console.log(array_pics)
+ 
 
-let change = window.addEventListener("click", imageChange);
 
-function imageChange(e){
-    console.log(e)
-//    const  card =document.querySelectorAll(".card") 
-    // const targ=e.srcElement.className;
-    const targ = e.srcElement.id;
-    console.log(targ);
-    if (targ =="card_1") {
-        targ.classList.add('.cardChange');
-        
+        // ====================================
+        const pics_name=[
+            "goku",
+            "gon",
+            "happy",
+            "luffy",
+            "naruto",
+            "shinichi",
+            "goku_2",
+            "gon_2",
+            "happy_2",
+            "luffy_2",
+            "naruto_2",
+            "shinichi_2"
+
+        ]
+
+    ;
+
+// =======================================container
+    const container ={
+    container:document.createElement("article")
     }
 
-// cardImgs.style.background = "blue";  
+    container.container.setAttribute("class",'main_container');
+    body.appendChild(container.container);
+// =================================================
+        function constructor(){
+            pics_name.forEach((pic)=>{
+            // console.log(pic);
+
+
+
+        const newDiv = {
+        card:document.createElement('div'),
+        
+
+
+        }
+
+        for (const [key,value] of Object.entries(newDiv)) {
+        // console.log(value);
+        value.setAttribute("id","")
+        value.setAttribute("class","card")
+        container.container.appendChild(value)
+
+        }    
+        })
+     
+    }
+    constructor()
+    
+ 
+    const cards= document.querySelectorAll('.card');
+    // console.log(cards);
+//     function display_pics(e){
+//         // console.log(e)
+    
+       
+//        cards.forEach((card) => {
+        
+//             card.innerHTML = array_pics.map(pic =>
+           
+                
+//             `
+//                         <img src=${pic}></img>
+    
+    
+//             `   
+            
+//             )
+//             })
+// }
+// display_pics();
+
+    let element=[]
+    console.log(element);
+for (let i = 0; i <cards.length; i++) {
+    // console.log(cards[i]);
+    element.push(cards[i]);
+    // console.log(element);
+    
+    
 }
-imageChange();
+
+const random = Math.floor(Math.random()*array_pics.length);
+console.log(array_pics[random]);
+async function addCards(){
+
+    element[0].innerHTML+= Math.floor(Math.random()*array_pics.length);
+    console.log(array_pics[random]);` 
+    
+                        <img src=${array_pics[random]}></img>
+        
+        
+        `
+    element[1].innerHTML+=` 
+    
+                        <img src=${array_pics[1]}></img>
+        
+        
+        `
+    element[2].innerHTML+=` 
+    
+                        <img src=${array_pics[2]}></img>
+        
+        
+        `
+    element[3].innerHTML+=` 
+    
+                        <img src=${array_pics[3]}></img>
+        
+        
+        `
+    element[4].innerHTML+=` 
+    
+                        <img src=${array_pics[4]}></img>
+        
+        
+        `
+    element[5].innerHTML+=` 
+    
+                        <img src=${array_pics[5]}></img>
+        
+        
+        `
+    element[6].innerHTML+=` 
+    
+                        <img src=${array_pics[6]}></img>
+        
+        
+        `
+    element[7].innerHTML+=` 
+    
+                        <img src=${array_pics[7]}></img>
+        
+        
+        `
+    element[8].innerHTML+=` 
+    
+                        <img src=${array_pics[8]}></img>
+        
+        
+        `
+    element[9].innerHTML+= 
+    
+    ` 
+    
+                        <img src=${array_pics[9]}></img>
+        
+        
+        `
+    element[10].innerHTML+=await ` 
+    
+                        <img src=${array_pics[10]}></img>
+        
+        
+        `
+    element[11].innerHTML+= 
+    ` 
+    
+                        <img src=${array_pics[11]}></img>
+        
+        
+    `
+    
+    // element[12].innerHTML+=` 
+    
+    //                     <img src=${array_pics[11]}></img>
+        
+        
+    //     `
+};
+addCards()
+
+// function randomCards(){
+//     for(let i = 0; i < cards.length; i++){
+//         cards[i].Math.random();
+//     }
+// }
+// randomCards();
+
+// function randomCards(){
+
+//     for(let i = 0; i < array_pics.length; i++){
+//         console.log(array_pics[i]);
+//         // cards[i].src= array_pics[i] ;
+
+//     } 
+// }
+// randomCards();
 
 
-// cardImgs.forEach(cardImg => {
-//     cardImg.addEventListener("click", imageChange, {once : true});
-// })
-function displayImage(){
+// function randomize(array_pics){
+//     let  i;
+//     let j;
+//     let tmp;
+//     for(i = array_pics.length-1;i>0;i--){
+//         j=math.floor(math.random()* (i+1));
+//         tmp=array_pics[i];
+//         array_pics[i]=array_pics[j];
+//         array_pics[j]=tmp
+//     }
+//     return array_pics;
+// }
+// array_pics.Math.random(8)
+// function randomMain(){
+//     let x = Math.floor((Math.random()*12));
+//     console.log(x);
+//     for(let i = 0; i < cards.length; i++){
+//         cards[i].x;
+//     }
+//     // if(window.location.reload){
+       
+//     // }
+    
+// }
+// randomMain();
 
-}
-
-displayImage()

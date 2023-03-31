eb2be0c96e8960cacf104e2764c5ec50a8db5ca4
calixte <calixtedeveloppeur@gmail.com>
2023-03-31 11:50:00 +0200

    commit3

--- index.js
+++ index.js
@@ -125,43 +125,49 @@ console.log(array_pics);
 
         }
 
-        for (const [key,value] of Object.entries(newDiv)) {
-        // console.log(value);
-        value.setAttribute("id",counter++)
-        value.setAttribute("class","card")
-        container.container.appendChild(value)
+        // for (const [key,value] of Object.entries(newDiv)) {
+        // // console.log(value);
+        // value.setAttribute("id",counter++)
+        // value.setAttribute("class","card")
+        // container.container.appendChild(value)
 
-        }    
+        // }    
         })
      
     }
     constructor()
     
  
-    const cards= document.querySelectorAll('.card');
-    // console.log(cards);
+const cards = document.querySelectorAll('.card');
+    const containerB=document.querySelector('.main_container')
+    console.log(containerB);
     // ====================================================
+
     function display_pics(e){
         // console.log(e)
        
-       
-        cards.forEach((card => {
+        for (let i = 0; i < pics_name.length; i++) {
+            const element = pics_name[i];
+            containerB.innerHTML = array_pics.map((p) =>
             
-            card.innerHTML = array_pics.map((pic)=>
-            console.log(pic)
+            ` 
+                   <div class="card">
+                        <img src=${p} alt="">
 
-            `
-                        <img src=${
-                Object.entries(pic)
-                        }>
-                        
-                        </img>
-    
-            `   
+                   </div>
             
-            )
-            })
-        )}
+            
+            `
+            
+            
+            
+            ).join("") 
+        }
+       
+            
+    
+          
+    }
 // display_pics();
 
     let element=[]
@@ -180,128 +186,11 @@ console.log(array_pics);
 // function addCards(){
 
 
-    
-//     element[0].innerHTML= 
-//         ` 
- 
-//                         <img src=${array_pics[0].pic}></img>
-        
-        
-//         `
-//     element[1].innerHTML=` 
-    
-//                         <img src=${array_pics[1].pic}></img>
-        
-        
-//         `
-//     element[2].innerHTML=` 
-    
-//                         <img src=${array_pics[2].pic}></img>
-        
-        
-//         `
-//     element[3].innerHTML=` 
-    
-//                         <img src=${array_pics[3].pic}></img>
-        
-        
-//         `
-//     element[4].innerHTML=` 
-    
-//                         <img src=${array_pics[4].pic}></img>
-        
-        
-//         `
-//     element[5].innerHTML=` 
 
-//                         <img src=${array_pics[5].pic}></img>
-        
-        
-//         `
-//     element[6].innerHTML=` 
-    
-//                         <img src=${array_pics[6].pic}></img>
-        
-        
-//         `
-//     element[7].innerHTML=` 
-    
-//                         <img src=${array_pics[7].pic}></img>
-        
-        
-//         `
-//     element[8].innerHTML=` 
-    
-//                         <img src=${array_pics[8].pic}></img>
-        
-        
-//         `
-//     element[9].innerHTML= 
-    
-//     ` 
-    
-//                         <img src=${array_pics[9].pic}></img>
-        
-        
-//         `
-//     element[10].innerHTML=` 
-    
-//                         <img src=${array_pics[10].pic}></img>
-        
-        
-//         `
-//     element[11].innerHTML= 
-//     ` 
-    
-//                         <img src=${array_pics[11].pic}></img>
-        
-        
-//     `
 
 // };
 // addCards()
 
-// function randomCards(){
-//     for(let i = 0; i < cards.length; i++){
-//         cards[i].Math.random();
-//     }
-// }
-// randomCards();
-
-// function randomCards(){
-
-//     for(let i = 0; i < array_pics.length; i++){
-//         console.log(array_pics[i]);
-//         // cards[i].src= array_pics[i] ;
-
-//     } 
-// }
-// randomCards();
 
 
-// function randomize(array_pics){
-//     let  i;
-//     let j;
-//     let tmp;
-//     for(i = array_pics.length-1;i>0;i--){
-//         j=math.floor(math.random()* (i+1));
-//         tmp=array_pics[i];
-//         array_pics[i]=array_pics[j];
-//         array_pics[j]=tmp
-//     }
-//     return array_pics;
-// }
-// array_pics.Math.random(8)
-// function randomMain(){
-//     let x = Math.floor((Math.random()*12));
-//     console.log(x);
-//     for(let i = 0; i < cards.length; i++){
-//         cards[i].x;
-//     }
-//     // if(window.location.reload){
-       
-//     // }
-    
-// }
-// randomMain();
 
